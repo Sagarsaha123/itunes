9c1a92cfbb3cae72df1a0ddc91c41a9058d13561
AnonymousX1025 <TheAnonymousX1025@gmail.com>
2023-08-06 14:01:49 +0000

    Auto Fixes

--- AnonXMusic/utils/stream/stream.py
+++ AnonXMusic/utils/stream/stream.py
@@ -9,7 +9,7 @@ from AnonXMusic import Carbon, YouTube, app
 from AnonXMusic.core.call import Anony
 from AnonXMusic.misc import db
 from AnonXMusic.utils.database import add_active_video_chat, is_active_chat
-from AnonXMusic.utils.exceptions import AssistantErr 
+from AnonXMusic.utils.exceptions import AssistantErr
 from AnonXMusic.utils.inline import close_markup, stream_markup, telegram_markup
 from AnonXMusic.utils.pastebin import AnonyBin
 from AnonXMusic.utils.stream.queue import put_queue, put_queue_index

64f435928aa1d9c0b24a6edc32cabd3db496e2a5
AnonymousX1025 <TheAnonymousX1025@gmail.com>
2023-08-06 16:05:52 +0000

    Auto Fixes

--- AnonXMusic/plugins/sudo/sudoers.py
+++ AnonXMusic/plugins/sudo/sudoers.py
@@ -4,9 +4,9 @@ from pyrogram.types import Message
 from AnonXMusic import app
 from AnonXMusic.misc import SUDOERS
 from AnonXMusic.utils.database import add_sudo, remove_sudo
-from AnonXMusic.utils.inline import close_markup
 from AnonXMusic.utils.decorators.language import language
 from AnonXMusic.utils.extraction import extract_user
+from AnonXMusic.utils.inline import close_markup
 from config import BANNED_USERS, OWNER_ID
 
 

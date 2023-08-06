158ad23341024c98610e0a61f61f0ff0e40a367f
˹ᴧɴσɴʏᴍσᴜs ꭙ˼ <TheAnonymousX1025@gmail.com>
2023-08-06 20:01:43 +0530

    Update resume.py

--- AnonXMusic/plugins/admins/resume.py
+++ AnonXMusic/plugins/admins/resume.py
@@ -3,6 +3,7 @@ from pyrogram.types import Message
 
 from AnonXMusic import app
 from AnonXMusic.core.call import Anony
+from AnonXMusic.utils.inline import close_markup
 from AnonXMusic.utils.database import is_music_playing, music_on
 from AnonXMusic.utils.decorators import AdminRightsCheck
 from config import BANNED_USERS
@@ -15,4 +16,4 @@ async def resume_com(cli, message: Message, _, chat_id):
         return await message.reply_text(_["admin_3"])
     await music_on(chat_id)
     await Anony.resume_stream(chat_id)
-    await message.reply_text(_["admin_4"].format(message.from_user.mention))
+    await message.reply_text(_["admin_4"].format(message.from_user.mention), reply_markup=close_markup(_))

c6afb01827fc732ff30a16b4705d11b38c5aa467
AnonymousX1025 <TheAnonymousX1025@gmail.com>
2023-08-06 15:33:31 +0000

    Auto Fixes

--- AnonXMusic/plugins/play/channel.py
+++ AnonXMusic/plugins/play/channel.py
@@ -44,8 +44,6 @@ async def playmode_(client, message: Message, _):
         except:
             return await message.reply_text(_["cplay_4"])
         if crid != message.from_user.id:
-            return await message.reply_text(
-                _["cplay_6"].format(chat.title, cusn)
-            )
+            return await message.reply_text(_["cplay_6"].format(chat.title, cusn))
         await set_cmode(message.chat.id, chat.id)
         return await message.reply_text(_["cplay_3"].format(chat.title, chat.id))

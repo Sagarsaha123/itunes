3fcdc8543a1f158c2e6d0be9aa18991b6cfeac08
AnonymousX1025 <TheAnonymousX1025@gmail.com>
2023-08-07 09:24:58 +0000

    Auto Fixes

--- AnonXMusic/utils/decorators/play.py
+++ AnonXMusic/utils/decorators/play.py
@@ -124,7 +124,9 @@ def PlayWrapper(command):
                     or get.status == ChatMemberStatus.RESTRICTED
                 ):
                     return await message.reply_text(
-                        _["call_2"].format(app.mention, userbot.id, userbot.name, userbot.username)
+                        _["call_2"].format(
+                            app.mention, userbot.id, userbot.name, userbot.username
+                        )
                     )
             except UserNotParticipant:
                 if chat_id in links:
@@ -150,9 +152,7 @@ def PlayWrapper(command):
                     invitelink = invitelink.replace(
                         "https://t.me/+", "https://t.me/joinchat/"
                     )
-                myu = await message.reply_text(
-                    _["call_4"].format(app.mention)
-                )
+                myu = await message.reply_text(_["call_4"].format(app.mention))
                 try:
                     await asyncio.sleep(1)
                     await userbot.join_chat(invitelink)
@@ -164,9 +164,7 @@ def PlayWrapper(command):
                             _["call_3"].format(app.mention, type(e).__name__)
                         )
                     await asyncio.sleep(3)
-                    await myu.edit(
-                        _["call_5"].format(app.mention)
-                    )
+                    await myu.edit(_["call_5"].format(app.mention))
                 except UserAlreadyParticipant:
                     pass
                 except Exception as e:

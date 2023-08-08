23ee0ab8f15d8deb79d716a1d14af08d9269b2eb
˹ᴧɴσɴʏᴍσᴜs ꭙ˼ <TheAnonymousX1025@gmail.com>
2023-08-08 19:23:47 +0530

    Update bot.py

--- AnonXMusic/core/bot.py
+++ AnonXMusic/core/bot.py
@@ -51,3 +51,6 @@ class Anony(Client):
             )
             exit()
         LOGGER(__name__).info(f"Music Bot Started as {self.name}")
+    async def stop(self):
+        await super().stop()
+

3db245c079193940e2ce0466abb576a29cfff24e
AnonymousX1025 <TheAnonymousX1025@gmail.com>
2023-08-16 18:00:45 +0000

    Auto Fixes

--- AnonXMusic/plugins/bot/help.py
+++ AnonXMusic/plugins/bot/help.py
@@ -4,7 +4,6 @@ from pyrogram import filters, types
 from pyrogram.types import InlineKeyboardMarkup, Message
 
 from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
 from AnonXMusic.utils import help_pannel
 from AnonXMusic.utils.database import get_lang
 from AnonXMusic.utils.decorators.language import LanguageStart, languageCB

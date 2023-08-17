24ccce104e3fe469fd2f1f9d1285536a8573ed0b
FALLEN <134856609+BtwFallen@users.noreply.github.com>
2023-08-17 06:01:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,11 +32,11 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/BtwFallen/Umm",
+    "https://github.com/AnonymousX1025/AnonXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
-    "GIT_TOKEN", "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"
+    "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")

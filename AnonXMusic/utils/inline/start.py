c3fdb4200ba9b1ec35a3c28b5b48901b69bade31
SATYAM <126665079+Raichuop07@users.noreply.github.com>
2023-08-18 01:49:33 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -1,37 +1,36 @@
+from config import SUPPORT_HEHE
 from pyrogram.types import InlineKeyboardButton
 
-import config
-from AnonXMusic import app
 
-
-def start_panel(_):
+def start_pannel(BOT_USERNAME):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
-            ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-        ],
-    ]
-    return buttons
-
-
-def private_panel(_):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
+                text="➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=new",
             )
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text="˹ʜᴇʟᴘ ᴍᴇɴᴜ˼", url=f"https://t.me/{BOT_USERNAME}?start=help"),
+            InlineKeyboardButton(text="⚙ sᴇᴛᴛɪɴɢs", callback_data="settings_helper"),
+        ],
+        ]
+    return buttons
+
+def private_panel(BOT_USERNAME):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=new",
+            )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=config.UPSTREAM_REPO),
+            InlineKeyboardButton(text="˹ᴜᴘᴅᴀᴛᴇꜱ˼", url="https://t.me/MOONLIGHT_SAGA"),
+            InlineKeyboardButton(text="˹ꜱᴜᴘᴘᴏʀᴛ˼", url=f"https://t.me/{SUPPORT_HEHE}")
         ],
+        [
+            InlineKeyboardButton(text="˹ʜᴇʟᴘ ᴍᴇɴᴜ˼", callback_data="settings_back_helper"),
+        ]
     ]
     return buttons

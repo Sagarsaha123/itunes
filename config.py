8579837989a0f204dc3bd959c2cd33947abbaa44
SATYAM <126665079+Raichuop07@users.noreply.github.com>
2023-08-18 15:42:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,6 +20,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
+LOG_ID = int(getenv("LOG_ID", None))
 
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", "5835406726"))

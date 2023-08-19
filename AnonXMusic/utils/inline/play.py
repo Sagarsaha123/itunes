36a46453d90fe40f186c006f3e194337896484f4
SATYAM <126665079+Raichuop07@users.noreply.github.com>
2023-08-19 13:03:21 +0530

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -65,7 +65,7 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ]
+        ],
         [
             InlineKeyboardButton(text="» sᴘᴇᴇᴅ »", callback_data=f"PanelMarkup {videoid}|{chat_id}"),
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")

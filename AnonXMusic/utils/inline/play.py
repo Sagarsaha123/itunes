abc54e871417850c44743f1179893237301af621
SATYAM <126665079+Raichuop07@users.noreply.github.com>
2023-08-19 12:04:55 +0530

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -51,7 +51,7 @@ def stream_markup_timer(_, chat_id, played, dur):
     elif 80 <= umm < 95:
         bar = "————————♪—"
     else:
-        bar = "—————————♪"
+        bar = "๏ 𝖥ɪɴɪsʜᴇᴅ ๏"
     buttons = [
         [
             InlineKeyboardButton(

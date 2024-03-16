b5cf2a6144b752525e55eca13f29eaa5284008f2
Priyanshu <159748687+priyanshurajop@users.noreply.github.com>
2024-03-16 18:54:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Raichuop07/Itunes",
+    "https://github.com/priyanshurajop/itunes",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
